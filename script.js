d969c63999fcb55182b7420dfb010984a7eb66f1
farahishaqat <70060032+farahishaqat@users.noreply.github.com>
2020-10-08 09:38:05 +0300

    final

--- script.js
+++ script.js
@@ -1,95 +1,206 @@
 
-//on click save take imputs and save them into variables
-// $('#save').click(function(){
-//    var yeildArray =[]
-//    yeildArray.push($("#yeild").val());
-//    var authorArray =[];
-//    authorArray.push($("#author").val()); 
-//    console.log(yeildArray)
-//    console.log(authorArray);
-// })
+//on click save1 take imputs and save them into variables
 
-
-$('#save').click(function(){
+$('#save1').click(function(){
    var newRecipe ={
+    date:"",
    	yeild:"" , 
    	author:"", 
    	prepTime:"", 
    	categories:"",
-   	discription: " " 
+   	discription: "" 
    }
 
+
+   newRecipe.date=$("#date").val();
    newRecipe.yeild=$("#yeild").val();
    newRecipe.author=$("#author").val();
    newRecipe.prepTime=$("#prep-time").val();
    newRecipe.categories= $("#categories").val();
-   newRecipe.discription= $("#disc").val();
+   newRecipe.discription= $("#discription-box").val();
 
-   console.log(newRecipe)
+   console.log(newRecipe);
 
 })
 
 
 
-//=============
+// $('#save2').click(function(){
+//    var newRecipeInfo ={
+//     ingredients:"",
+//     instructions:"" , 
+//     notes:"", 
+//    }
+
+//    newRecipeInfo.ingredients=$("##add-ingredients-input").val();
+//    newRecipeInfo.instructions=$("#add-instructions-input").val();
+//    newRecipeInfo.notes=$("#add-notes-input").val();
+
+//    console.log(newRecipeInfo);
+
+// })
+
+//Edit button
+
+// var editButton= $("<button> Edit </button>")
+//   editButton.attr('id',"edit")
+//   $("<li>"+ editButton +"</li>").appendTo("#ingr-list")
+
+
 //Add section
 
 //add ingredients
 
+var ingredientsArray=[];
+var instructionsArray=[];
+var notesArray=[];
+
 $("#add-ingredients").click(function(){
-	var ingredients =$("#add-ingredients-input").val();
-	if(ingredients === ""){
-		alert("You must write something")
-	} else {
-	$("<li>"+ ingredients +"</li>").appendTo(".ordered-list")}
+  
+  var ingredients =$("#add-ingredients-input").val();
+  if(ingredients === ""){
+    alert("You must write something")
+  } else {
+  $("<li>"+ ingredients +"</li>").appendTo("#ingr-list")
+  ingredientsArray.push(ingredients);
+  console.log(ingredientsArray);
+
+}
+             
 })
 
 
-// $("#add-task").click(function(){
-// 	var task = $("#input-task").val()
-// 	if(task === ""){
-// 		alert("You must write something")
-// 	} else {
-// 	$("<li>"+ task +"</li>").appendTo("#todo-list").on("click", function toggle(){
-// 		$(this).toggleClass("crossed")}   )}
-// 	$("#input-task").val("");
-//    })
+// Empty ingredient input feild after adding an ingredient
+  $('#add-ingredients').click( function(){
+        if($("#add-ingredients-input").val() !== " "){
+        	//OR use .val().length === 0
+        	$("#add-ingredients-input").val("");}
+    })
 
 
-// $("#add-task").click(function(){
-// 	var task = $("#input-task").val()
-// 	if(task === ""){
-// 		alert("You must write something")
-// 	} else {
-// 	$("<li>"+ task +"</li>").appendTo("#todo-list").on("click", function toggle(){
-// 		$(this).toggleClass("crossed")}   )}
-// 	$("#input-task").val("");
-//    })
+//add instruction 
+
+$("#add-instructions").click(function(){
+	var instructions =$("#add-instructions-input").val();
+	if(instructions === ""){
+		alert("You must write something")
+	} else {
+	$("<li>"+ instructions +"</li>").appendTo("#inst-list")
+   instructionsArray.push(instructions);
+   console.log(instructionsArray);
+}           
+})
+
+// Empty instructions input feild after adding an instruction
+  $('#add-instructions').click( function(){
+        if($("#add-instructions-input").val() !== " "){
+        	//OR use .val().length === 0
+        	$("#add-instructions-input").val("");}
+    })
 
 
+//add notes
+
+$("#add-notes").click(function(){
+	var notes =$("#add-notes-input").val();
+	if(notes === ""){
+		alert("You must write something")
+	} else {
+	$("<li>"+ notes +"</li>").appendTo("#notes-list")
+ notesArray.push(notes);
+   console.log(notesArray);
+}          
+})
+
+// Empty notes input feild after adding a note
+  $('#add-notes').click( function(){
+        if($("#add-notes-input").val() !== " "){
+        	//OR use .val().length === 0
+        	$("#add-notes-input").val("");}
+    })
 
 
+// Delete Section 
  
-
-  $("#remove-task").click(function(){ 
-  	if ( $('#todo-list li').length === 0 ){
-  		alert("no more tasks to remove");
+// delete ingredient 
+$("#delete-ingredients").click(function(){ 
+  	if ( $('#ingr-list li').length === 0 ){
+  		alert("no more ingredients to remove");
   	}
   	else {
-  		$("#todo-list li:last-child").remove()
+  		$("#ingr-list li:last-child").remove()
+      ingredientsArray.pop();
+      console.log(ingredientsArray)
   	}
    })
 
 
-    // Empty input feild after adding a task
-  $('#add-task').click( function(){
-        if($("#input-task").val() !== " "){
-        	//OR use .val().length === 0
-        	$("#input-task").val("");}
-    })
+// delete instruction
 
-//remove all tasks
-    $(".input-container").append("<button id ='remove-all' > Remove all Tasks </button>")
+  $("#delete-instructions").click(function(){ 
+  	if ( $('#inst-list li').length === 0 ){
+  		alert("no more instructions to remove");
+  	}
+  	else {
+  		$("#inst-list li:last-child").remove()
+      instructionsArray.pop();
+      console.log(instructionsArray)
+  	}
+   })
 
-  $('#remove-all').click(function(){
-	$('ul').empty(); })
+
+// delete note
+
+    $("#delete-notes").click(function(){ 
+  	if ( $('#notes-list li').length === 0 ){
+  		alert("no more notes to remove");
+  	}
+  	else {
+  		$("#notes-list li:last-child").remove()
+      notesArray.pop();
+      console.log(notesArray)
+  	}
+   })
+
+
+
+//remove all section
+
+//remove all ingredients
+   
+  $('#delete-all-ingredients').click(function(){
+	$('#ingr-list').empty();
+  ingredientsArray.splice(0,ingredientsArray.length);
+
+  console.log(ingredientsArray);
+   })
+
+  
+
+  //remove all instructions
+   
+  $('#delete-all-instructions').click(function(){
+	$('#inst-list').empty()
+  instructionsArray.splice(0,instructionsArray.length);
+
+  console.log(instructionsArray)
+
+   })
+
+
+  //remove all notes
+   
+  $('#delete-all-notes').click(function(){
+	$('#notes-list').empty()
+  notesArray.splice(0,notesArray.length);
+
+  console.log(notesArray)
+
+})
+
+
+  // $("#reset-btn").click( function(){$("#Myform").trigger("reset")})
+
+ 
+        
+      
